1e3b1dcb55e7dfa0879a4275bafd6ac4883d346b
Mark Coleman <mark.coleman@outlook.com>
2020-04-16 12:26:26 +0100

    Exercise 4a - convert to upper case

---
+++ src/Ex4-Text/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex4_Text
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Please enter a sentence > ");
+            var text = Console.ReadLine();
+
+            Console.WriteLine(text.ToUpper());
+            Console.ReadLine();
+        }
+    }
+}
