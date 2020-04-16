6acdd0b2d4b373b15c4c147bb1ba26cd6cebdfe9
Mark Coleman <mark.coleman@outlook.com>
2020-04-16 12:09:40 +0100

    Exercise 2 - Loops and Conditions

---
+++ src/Ex2-LoopsAndConditions/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Ex2_NumbersAndColours
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 1; i < 31; i++)
+            {
+                if (i < 11)
+                {
+                    Console.WriteLine($"Small {i}");
+                }
+                else if (i < 21)
+                {
+                    Console.WriteLine($"Medium {i}");
+                }
+                else
+                {
+                    Console.WriteLine($"Large {i}");
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+}
