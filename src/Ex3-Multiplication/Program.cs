affcb9a84115ecab2abafd0872b3abdd8fd62b24
Mark Coleman <mark.coleman@outlook.com>
2020-04-16 12:17:25 +0100

    Exercise 3a - multiplication

---
+++ src/Ex3-Multiplication/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex3_Multiplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Please enter a number > ");
+            var input = Console.ReadLine();
+
+            int number = int.Parse(input);
+
+            for (int i = 1; i < 13; i++)
+            {
+                Console.WriteLine($"{number} * {i} = {number * i}");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
