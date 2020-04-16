f73554dc8533ed0110b1cc836a97ce4fae18a1f3
Mark Coleman <mark.coleman@outlook.com>
2020-04-16 11:54:11 +0100

    Exercise 1a - Hello world

---
+++ src/Ex1-HelloWorld/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Ex1_HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello world");
+            Console.ReadLine();
+        }
+    }
+}
